171883979fbce19e06cda24ce82e35e7244ed883
PlanetsLightningArrester <franciscogssm94@gmail.com>
2022-09-15 20:36:55 -0300

    add tests for rico notes

--- src/tests/updater.test.ts
+++
@@ -1,9 +0,0 @@
-
-import { AssetParser } from '../assetParser';
-import path from 'path';
-
-const assetsParser = new AssetParser(path.join(__dirname, '..', '..', 'assets.json'));
-
-test('simple crawler', async () => {
-  await assetsParser.getListedAssets();
-}, 5000*1000);
