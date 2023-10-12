9e3c25792de837938dd6c2365be112274dae2215
Planet's Lightning Arrester <franciscogssm94@gmail.com>
2023-10-12 13:49:24 -0300

    feat: add squirrel configs

--- backend/forge.config.js
+++ backend/forge.config.js
@@ -1,13 +1,27 @@
+const path = require('path');
+/** @type {import('@electron-forge/maker-squirrel').MakerSquirrelConfig} */
+const makerSquirrelConfig = {
+  title: "Leitor de notas de corretagem",
+  name: "leitor-de-notas-de-corretagem",
+  authors: "Planet's Lightning Arrester",
+  description: "Parse Brazilian PDF brokerage notes",
+  setupExe: "LeitorInstall.exe",
+  setupMsi: "LeitorInstall.msi",
+  setupIcon: path.join(__dirname, 'src', 'images', 'icon-setup.ico'),
+  loadingGif: path.join(__dirname, 'src', 'images', 'icon-setup.gif'),
+};
 /** @type {import('@electron-forge/shared-types').ForgeConfig} */
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: path.join(__dirname, 'src', 'images', 'icon'),
+    executableName: 'leitor',
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: makerSquirrelConfig,
     },
     {
       name: '@electron-forge/maker-zip',
