1baf4e55358d78104b0d04f04030ff1dbc5393dc
Planet's Lightning Arrester <franciscogssm94@gmail.com>
2024-01-09 00:11:00 -0300

    refactor(ops): remove electronegativity from electron-forge and add linter instead

--- backend/forge.config.ts
+++ backend/forge.config.ts
@@ -1,27 +1,18 @@
-import path from 'path';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { type ForgeConfig } from '@electron-forge/shared-types';
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
-import { ElectronegativityPlugin, type ElectronegativityConfig } from '@electron-forge/plugin-electronegativity';
-
-const electronNgConfig: ElectronegativityConfig = {
-  parserPlugins: [],
-  isSarif: true
-};
+import path from 'path'
+import { MakerZIP } from '@electron-forge/maker-zip'
+import { type ForgeConfig } from '@electron-forge/shared-types'
+import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives'
 
 const config: ForgeConfig = {
   packagerConfig: {
-    name: "Leitor de notas de corretagem",
+    name: 'Leitor de notas de corretagem',
     asar: true,
     icon: path.join(__dirname, 'src', 'images', 'icon'),
     executableName: 'leitor',
   },
   rebuildConfig: {},
-  makers: [new MakerZIP({}),],
-  plugins: [
-    new AutoUnpackNativesPlugin({}),
-    new ElectronegativityPlugin(electronNgConfig),
-  ],
-};
+  makers: [new MakerZIP({})],
+  plugins: [new AutoUnpackNativesPlugin({})],
+}
 
-export default config;
+export default config
